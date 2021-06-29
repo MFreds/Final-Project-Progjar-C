b51fb798ee982afd55de4be24d9eee093e223d8e
tux0112 <ironhidedjokam313@gmail.com>
2021-06-29 16:43:19 +0800

    create ready for find other client

--- Client.py
+++ Client.py
@@ -10,10 +10,39 @@ import select
 import sys
 from threading import Thread
 from ftplib import FTP
-
 ip_chat = ''
 port_chat = 8082
 
 #connect to server
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.connect((ip_chat, port_chat))
+
+# terima dari server
+def recv_msg(sock):
+	while True:
+		data = sock.recv(2048)
+		sys.stdout.write(data.decode() + '\n')
+
+# Thread(target=send_msg, args=(server,)).start()
+Thread(target=recv_msg, args=(server,)).start()
+
+while True:
+    
+    #this thing just testing
+    message="find"
+    server.send(message.encode())
+
+    #list server socket
+    socket_list=[server]
+    
+    # get from list
+    read_socket, write_socket, error_socket = select.select(socket_list, [], [])
+    
+    # Looping untuk menerima dan mengirim pesan
+    for socks in read_socket:
+        if socks == server:
+            recv_msg(socks)
+        # else:
+        #     send_msg(socks)
+    
+server.close()
