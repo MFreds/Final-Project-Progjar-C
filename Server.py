236f235d4732f38e081a688682d396f261dbde4d
tux0112 <ironhidedjokam313@gmail.com>
2021-06-29 17:31:53 +0800

    create chat feature

--- Server.py
+++ Server.py
@@ -16,15 +16,41 @@ server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 ip_address = ''
 port = 8082
 server.bind((ip_address, port))
-server.listen(1)
+server.listen(2)
 list_of_clients = []
 ready_client = []
+ready_client_addr = []
 
+# send message to other client
+def kirimpesan(message, connection):
+    for clients in ready_client :
+        if clients != connection:
+            try:
+                clients.send(message.encode())
+            except:
+                clients.close()
+
+#insert founded conversation into one room
+def inroom(conn,addr):
+    # print("aloha again again there!!!")
+    while True:
+        try:
+            data = conn.recv(2048).decode()
+            print(data)
+            message_to_send =  data
+            kirimpesan(message_to_send, conn)
+            
+            
+        except:
+            continue
+
+#client waiting for other client to conversation
 def readyclient(conn,addr):
     # print("hi there its me")
 #insert the new ready client to list and calculate size of list
 
     ready_client.append(conn)
+    ready_client_addr.append(addr)
     size_ready_client=0
     for y in ready_client:
         
@@ -33,6 +59,14 @@ def readyclient(conn,addr):
             size_ready_client+=1
             print("sum of ready client is: ",size_ready_client+1)
             
+            if size_ready_client==1:
+                
+                # print("aloha there!!!")
+                for i in range(2):
+                    
+                    # print("aloha again there!!!")
+                    inroom(ready_client[i], ready_client_addr[i])
+                
            
     # print("aloha")
     message="waiting for other"
