fd9abbb147720ec8a46d2c9fc3f73fcfd437102d
tux0112 <ironhidedjokam313@gmail.com>
2021-06-30 19:23:21 +0800

    change port and add explanation in comment

--- Client.py
+++ Client.py
@@ -10,7 +10,7 @@ import select
 import sys
 from threading import Thread
 ip_chat = ''
-port_chat = 8080
+port_chat = 8081
 
 #connect to server
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -21,7 +21,7 @@ def send_msg(sock):
     while True:
         message = input()
         
-        sock.send(message.encode())
+        sock.send(message.encode()) 
         sys.stdout.write('<You> ')
         sys.stdout.write(message + '\n')
         sys.stdout.flush()
@@ -37,24 +37,34 @@ def recv_msg(sock):
 Thread(target=send_msg, args=(server,)).start()
 Thread(target=recv_msg, args=(server,)).start()
 
+
+def ready():
+    #this thing just testing
+    message = input()
+    server.send(message.encode())
+
 while True:  
+    
+    ready()
+    
     #list server socket
     socket_list=[server]
     # socket_list=[sys.stdin]
     
     # get from list
-    # read_socket, write_socket, error_socket = select.select(socket_list, [], [])
-    read_socket, write_socket, error_socket = select.select([sys.stdin], [], [],0)
     
-    #this thing just testing
-    message = input()
-    server.send(message.encode())
+    ########## uncomment the code for using
+    ########## for linux  ##########
+    read_socket, write_socket, error_socket = select.select(socket_list, [], [],0)
+    
+    ########## for windows #########
+    # read_socket, write_socket, error_socket = select.select([socket _list], [], [])
     
     # Looping untuk menerima dan mengirim pesan
     for socks in read_socket:
         if socks == server:
             recv_msg(socks)
-        else:
-            send_msg(socks)
+        # else:
+        #     send_msg(socks)
     
 server.close()
